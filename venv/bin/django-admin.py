b9ac63e99f2cc4a465043b0ec59708dedb67efcf
PradeepOff <nagavarapradeepyendluri@gmail.com>
2019-11-14 16:25:11 +0530

    Project Creation

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pradeepyendluri/Desktop/booKeeper/Citric/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/lib/python3.7/ntpath.py
@@ -0,0 +1,1 @@
+/home/pradeepyendluri/anaconda3/lib/python3.7/ntpath.py
